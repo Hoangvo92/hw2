9a6d241bcd5a15d29d791bd3a7a2c6dd7a277f3d
Hoang D Vo <hoangvod@gmail.com>
2019-10-04 03:50:25 -0500

    Update main.cpp

--- Homework2/Homework2/main.cpp
+++ Homework2/Homework2/main.cpp
@@ -28,9 +28,6 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
-//my include of library
-
-
 
 #if __APPLE__
 #include <GLUT/glut.h>
@@ -52,51 +49,51 @@ float shininess[] = { 50.0 };
 
 
 void rotatePot(GLfloat x, GLfloat y, GLfloat z, GLfloat size, GLfloat angle) {
+	/*Input: x,y,z - the cordinate of the teapot
+	         size - the size of the teapot
+			 angle - the rotated angle of the teapot around the z-xis, in degree
+			         for the glRotatef
+	output: nothing
+	function: draw the teapot with rotated angle around z-axis
+	*/
 	glPushMatrix();
+	//locate the location of the the teapot
 	glTranslatef(x, y, z);
-	//glRotatef(angle, x, y, z); // in this one, the pot will self-rotate while do up-right
-	glRotatef(angle, 0, 0, 1);
-	glutSolidTeapot(size);
+	//glRotatef(angle, x, y, z); // in this one, the pot will self-rotate  around z-axis, x-axis, and y-axis
+	//rotate with the assigned angle, (x,y,z)=(0,0,1)->rotate around z-xis
+	glRotatef(angle, 0, 0, 1);//the angle is in degree, not radian
+	glutSolidTeapot(size);//draw teapot
 	glPopMatrix();
 }
 
 
 void problem1() {
-
-	// TODO: Your code here!
-	// I choose this code because it looks like the requirement, but I prefer the one in the comment
-	//this code is for only this specific case 1
-	GLfloat size = 0.2;
-	float pi = 3.14;
-	GLfloat xarray[10] = { cos(0), cos(pi / 6) , cos(pi / 3), cos(2*pi/3), cos( 5*pi/6),
-	                      cos(pi), cos(7*pi/6), cos(4*pi/3), cos(5*pi/3), cos(11*pi/6) };
-	GLfloat yarray[10] = { sin(0), sin(pi / 6) , sin(pi / 3), sin(2*pi/3), sin( 5*pi/6),
-	                      sin(pi), sin(7*pi/6), sin(4*pi/3), sin(5*pi/3), sin(11*pi/6) };
-	GLfloat angle [10] = {0, 30, 60, 120, 150, 180, 210, 240, 300, 330};
-
-	for (int i = 0; i < 10; i++) {
-	 rotatePot(xarray[i]*1.5, yarray[i]*1.5, 0, size, angle[i]);
-	}
-
-	/*
-	//this section is for general case, easy to adjust
-	float pi = 3.1415;
-	GLfloat size = 0.22;
-	GLfloat angle = 0;
-	GLfloat angle2 = 0;
-	GLfloat add = pi / 6;
-	GLfloat x, y, z;
-	while (angle < 12*add) {
-		if ((angle != (3*add)) && (angle != (9*add))) {
-			z = 0;
-			x = cos(angle);
-			y = sin(angle);
-			rotatePot(x, y, z, size, angle2);
-		}
-		angle += add;
-		angle2 += 30;
-	}
+	/*Input: nothing
+	  output: nothing
+	  function: draw just like a picture
 	*/
+	// TODO: Your code here!
+
+	//declare pi
+	float pi = 3.1415f;
+	//assign a random size for the teapot
+	GLfloat size = 0.22f;
+	//the first angle in both radian and degree
+	GLfloat angle = 0.0f;
+	GLfloat angle2 = 0.0f;
+	//the different angle between each teapot= 2/10*pi= 1/5*pi=1/5*180
+	GLfloat add = pi / 5;
+	GLfloat add2 = (180.0f / 5);//for the glRotatef()
+	//the dummy variables to store cordinate of each teapot
+	GLfloat x, y, z=0;
+	while (angle < 2*pi) {
+		x = cos(angle); //calculate value of x-ordinate
+		y = sin(angle); //calculate value of y-ordinate
+		rotatePot(x, y, z, size, angle2);//call out the function
+		angle += add;//increase angle in radian
+		angle2 += add2;//increase the angle in degree
+	}
+	return;
 }
 //function drawCollumn is used in problem2()
 void drawCollumn( GLfloat x, GLfloat y, GLfloat z, GLfloat width, GLfloat height) {
@@ -105,12 +102,10 @@ void drawCollumn( GLfloat x, GLfloat y, GLfloat z, GLfloat width, GLfloat height
 	  Output: nothing
 	  Function: drawing a cube with scale (width, height, length) at (x,y,z)*/
 	glPushMatrix();
-	glTranslatef(x, y, z);
-	glScalef(width, height, width );
+	glTranslatef(x, y, z);//put the collumn at the location (x, y, z)
+	glScalef(width, height, width ); //scale the cube into collumns
 	glutSolidCube(1);
 	glPopMatrix();
-
-
 
  }
 
@@ -118,22 +113,23 @@ void problem2() {
 	// TODO: Your code here!
 
 	//values to scale the cubes
-	GLfloat width = 0.2;  // the width and length of the cubes
-	GLfloat height = 0.1; //the height of the cubes, increasing
+	GLfloat width = 0.2f;  // the width and length of the cubes
+	GLfloat height = 0.1f; //the height of the cubes, increasing
 	//The cordinates
-	GLfloat xfirst = -2; //the first location of x
-	GLfloat dif = 0.04; //the difference in height between two adjacent collumns
+	GLfloat xfirst = -2.0f; //the first location of x
+	GLfloat dif = 0.04f; //the difference in height between two adjacent collumns
 	GLfloat addDif = dif/2; //use addDif to help all collumns stay equally on a flat surface
-	GLfloat xlate = 2; //the end of the line
-	GLfloat yfirst = 0; //the first value of y =0, the first box
+	GLfloat xlate = 2.0f; //the end of the line
+	GLfloat yfirst = 0.0f; //the first value of y =0, the first box
 	//while run from xfirst to xlate x(-2, 2)
 	while (xfirst < xlate) 
 	{
-		drawCollumn(xfirst, yfirst, 0, width, height); //draw a cube on xOy surface, z=0
+		drawCollumn(xfirst, yfirst, 0.0f, width, height); //draw a cube on xOy surface, z=0
 		xfirst += width; //each cube-center is |width| unit from each other on x-axis
 		yfirst += addDif; //add 1/2 the difference in height to help all cubes stay on one surface
 		height += dif; //increase the height
 	}	
+	return;
 }
 
 void transTeapot(GLfloat x, GLfloat y, GLfloat z, GLfloat size) {
@@ -151,10 +147,10 @@ void transTeapot(GLfloat x, GLfloat y, GLfloat z, GLfloat size) {
 void problem3() {
 	// TODO: Your code here!
 	//the cordinate of the peak teapot-constant
-	GLfloat xfirst = 0;
-	GLfloat yfirst = 0.75;
+	GLfloat xfirst = 0.0f;
+	GLfloat yfirst = 0.75f;
 	//the size for each teapot
-	GLfloat size = 0.1;
+	GLfloat size = 0.1f;
 	//the (xmov, ymove) is coordinate of each teapot-i, the teapot-1 has cordinate (0, 0.75, 0)
 	GLfloat xmov = xfirst;
 	GLfloat ymov = yfirst;
@@ -162,7 +158,7 @@ void problem3() {
 	GLfloat xw, yw;
 	int cal = 0; //use to locate the odd and even row on the eramic
 	//draw the first teapot at the peak
-	transTeapot(xfirst, yfirst, 0, size);
+	transTeapot(xfirst, yfirst, 0.0f, size);
 	//Do while loop, while x < 1.5, in the line y = 0.75 - x
 	while (xmov <= (2 * yfirst )) {
 		ymov = ymov - 2*size; //decrease y by 2 time the size of teapot- new ymov
@@ -171,110 +167,132 @@ void problem3() {
 		if (cal == 0) {
 			xw = 2*size; //begin drawing from the location (2*size, ymov,0) and the symetric counterpart
 			yw = ymov;
-			transTeapot(xw, yw, 0, size);
-			transTeapot(-xw, yw, 0, size);
+			transTeapot(xw, yw, 0.0f, size);
+			transTeapot(-xw, yw, 0.0f, size);
 			cal = 1;
 		}
 		else
 		{//if the row is odd
-			xw = 0;
+			xw = 0.0f;
 			yw = ymov;
-			transTeapot(xw, yw, 0, size); //begin drawing at the center of the row
+			transTeapot(xw, yw, 0.0f, size); //begin drawing at the center of the row
 			cal = 0;
 		}
 		while (xw <= xmov) {
 			xw = xw + 4 * size;
 			if (xw > xmov) //to avoid drawing outside of the line
 			{
-				transTeapot(xmov, yw, 0, size);
-				transTeapot(-xmov, yw, 0, size);
+				transTeapot(xmov, yw, 0.0f, size);
+				transTeapot(-xmov, yw, 0.0f, size);
 			}
 			else
 			{//draw teapot in two sides of the row
-				transTeapot(xw, yw, 0, size);
-				transTeapot(-xw, yw, 0, size);
+				transTeapot(xw, yw, 0.0f, size);
+				transTeapot(-xw, yw, 0.0f, size);
 			}	
 		}	
 	}
+	return;
 }
-void drawStar(GLfloat x, GLfloat y,  GLfloat distance)
+//function to draw the wing-sign of victory
+void drawVictory(GLfloat x, GLfloat y,  GLfloat distance)
 {
+	/*Input: the (x,y) coordinate of the center of the wing
+	         distance- a numerical measure to locate other points of the wing
+	  output: nothing
+	  function: draw a pair of wings*/
 	glBegin(GL_LINE_LOOP);
-	glVertex2f(x, y);
-	glVertex2f(x + 1 / 2 * distance, y + 3 / 2 * distance);
+	glVertex3f(x, y, 0.0f);
+	glVertex3f(x + distance, y + 1.5 * distance, 0.0f);
+	glVertex3f(x + 2 * distance, y - 0.5 * distance, 0.0f);
+	glVertex3f(x + distance, y + 1.5 * distance-0.05, 0.0f);
+	glVertex3f(x, y - 0.05, 0.0f);
+	glVertex3f(x - distance, y + 1.5 * distance - 0.05, 0.0f);
+	glVertex3f(x - 2 * distance, y - 0.5 * distance, 0.0f);
+	glVertex3f(x -  distance, y + 1.5 * distance, 0.0f);
+	glEnd();
 	
-	glVertex2f(x, y + distance);
-	glVertex2f(x + distance, y);
-	glVertex2f(x + distance, y + distance);
 
+}
+//function to draw hand part 1
+void part1() {
+	//twist
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(-0.1f, -0.4f, 0.0f);
+	glVertex3f(-0.1f, -0.25f, 0.0f);
+	glVertex3f(0.1f, -0.25f, 0.0f);
+	glVertex3f(0.1f, -0.4f, 0.0f);
+	glEnd();
+}
+//function to draw hand part 2
+void part2() {
+	//thumb finger
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(0.1f, -0.25f, 0.0f);
+	glVertex3f(0.15f, -0.1f, 0.0f);
+	glVertex3f(0.08f, 0.0f, 0.0f);
+	glVertex3f(0.0f, -0.05f, 0.0f);
+	glVertex3f(-0.05f, -0.1f, 0.0f);
+	glVertex3f(0.07f, -0.1f, 0.0f);
+	glVertex3f(0.1f, -0.2f, 0.0f);
+	glEnd();
+}
+//functio to draw hand part 3
+void part3() {
+	//pointy finger and middle finger
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.08f, 0.0f, 0.0f);
+	glVertex3f(0.1f, -0.05f, 0.0f);
+	glVertex3f(0.15f, 0.2f, 0.0f);
+	glVertex3f(0.1f, 0.2f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-0.1f, 0.2f, 0.0f);
+	glVertex3f(-0.15f, 0.2f, 0.0f);
+	glVertex3f(-0.1f, 0.05f, 0.0f);
+	glVertex3f(0.0f, -0.1f, 0.0f);
+	glEnd();
+}
+//function to draw hand part 4
+void part4() {
+	//ring finger
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(-0.1f, 0.05f, 0.0f);
+	glVertex3f(0.0f, -0.1f, 0.0f);
+	glVertex3f(-0.03f, -0.13f, 0.0f);
+	glVertex3f(-0.15f, 0.03f, 0.0f);
+	glEnd();
+}
+//function to draw hand part 5
+void part5() {
+	//final finger
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(-0.15f, 0.03f, 0.0f);
+	glVertex3f(-0.2f, 0.0f, 0.0f);
+	glVertex3f(-0.1f, -0.15f, 0.0f);
+	glVertex3f(-0.07f, -0.14f, 0.0f);
 	glEnd();
 
+	//complete the hand
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(-0.15f, -0.1f, 0.0f);
+	glVertex3f(-0.1f, -0.25f, 0.0f);
+	glEnd();
 }
 void problem4() {
 	// TODO: Your code here!
-	//twist
+	//Note: problem2 can affect the default color setting
+	//draw the hand with V sign
 	glPushMatrix();
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(-0.1, -0.4, 0.0);
-	glVertex3f(-0.1, -0.25, 0.0);
-	glVertex3f(0.1, -0.25, 0.0);
-	glVertex3f(0.1, -0.4, 0.0);
-	glEnd();
+	glColorMaterial(GL_FRONT, GL_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);
+	glPixelTransferf(GL_RED_BIAS, 0.1);
+	part1(); part2(); part3(); part4(); part5();
+	//draw victory
+	drawVictory(0.0f, 0.5f, 0.5f);
 	glPopMatrix();
-	//thumb finger
-	glPushMatrix();
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(0.1, -0.25, 0.0);
-	glVertex3f(0.15, -0.1, 0.0);
-	glVertex3f(0.08, 0.0, 0.0);
-	glVertex3f(0.0, -0.05, 0.0);
-	glVertex3f(-0.05, -0.1, 0.0);
-	glVertex3f(0.07, -0.1, 0.0);
-	glVertex3f(0.1, -0.2, 0.0);
-	glEnd();
-	glPopMatrix();
-	//pointy finger and middle finger
-	glPushMatrix();
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(0.08, 0.0, 0.0);
-	glVertex3f(0.1, -0.05, 0.0);
-	glVertex3f(0.15, 0.2, 0.0);
-	glVertex3f(0.1, 0.2, 0.0);
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(-0.1, 0.2, 0.0);
-	glVertex3f(-0.15, 0.2, 0.0);
-	glVertex3f(-0.1, 0.05, 0.0);
-	glVertex3f(0.0, -0.1, 0.0);
-	glEnd();
-	glPopMatrix();
-	//ring finger
-	glPushMatrix();
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(-0.1, 0.05, 0.0);
-	glVertex3f(0.0, -0.1, 0.0);
-	glVertex3f(-0.03, -0.13, 0.0);
-	glVertex3f(-0.15, 0.03, 0.0);
-	glEnd();
-	glPopMatrix();
-	//final fingr
-	glPushMatrix();
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(-0.15, 0.03, 0.0);
-	glVertex3f(-0.2, 0.0, 0.0);
-	glVertex3f(-0.1, -0.15, 0.0);
-	glVertex3f(-0.07, -0.14, 0.0);
-	glEnd();
-	glPopMatrix();
-	//complete the hand
-	glPushMatrix();
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(-0.15, -0.1, 0.0);
-	glVertex3f(-0.1, -0.25, 0.0);
-	glEnd();
-	glPopMatrix();
+	glFlush();
 
-	
 }
 
 void display() {
